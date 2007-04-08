d4f09bee1f4d35cce822b2db328b5f846824dc94
c95029 <c95029@2bd7e31b-6d2d-0410-b585-a5d07697af20>
2007-04-08 17:53:24 +0000

    WelcomeFileList追加

--- SDLoader/src/main/java/sdloader/j2ee/webxml/WebXmlParseHandler.java
+++ SDLoader/src/main/java/sdloader/j2ee/webxml/WebXmlParseHandler.java
@@ -53,6 +53,7 @@ public class WebXmlParseHandler extends DefaultHandler {
 		tag2classMap.put("servlet", ServletTag.class);
 		tag2classMap.put("servlet-mapping", ServletMappingTag.class);
 		tag2classMap.put("init-param", InitParamTag.class);
+		tag2classMap.put("welcome-file-list", WelcomeFileListTag.class);
 	}
 
 	private Map resolveMap = new HashMap();
