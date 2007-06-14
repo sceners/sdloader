c25399dfe39f8ced17504d34263e71baab85655d
shinpei.ohtani <shinpei.ohtani@2bd7e31b-6d2d-0410-b585-a5d07697af20>
2007-06-14 17:21:54 +0000

    SessionManagerの実クラスを切り離した。 ロードの仕組みはまた別途考えるが今はシンプルな仕組みとした。

---
+++ trunk/SDLoader/src/main/java/sdloader/internal/SDLoaderInitializer.java
@@ -0,0 +1,26 @@
+package sdloader.internal;
+
+import sdloader.j2ee.SessionManager;
+import sdloader.j2ee.imp.SessionManagerSharedSessionImp;
+
+/**
+ * @author shot
+ */
+public abstract class SDLoaderInitializer {
+
+	private static SessionManager sessionManager;
+	
+	//TODO SDLoaderの初期化処理の整理・分離
+	
+	//TODO もっと汎用的なロードの仕組みを作る
+	public static SessionManager createSessionManager() {
+		if(sessionManager != null) {
+			return sessionManager;
+		}
+		return new SessionManagerSharedSessionImp();
+	}
+	
+	public static void setSessionManager(SessionManager sessionManager) {
+		SDLoaderInitializer.sessionManager = sessionManager;
+	}
+}
