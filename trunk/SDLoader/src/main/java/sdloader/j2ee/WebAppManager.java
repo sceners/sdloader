88dc73af14092f591508012964cdba9df572f51d
c95029 <c95029@2bd7e31b-6d2d-0410-b585-a5d07697af20>
2007-08-22 02:08:52 +0000

    ルートに配置したファイルを読めるように修正

--- trunk/SDLoader/src/main/java/sdloader/j2ee/WebAppManager.java
+++ trunk/SDLoader/src/main/java/sdloader/j2ee/WebAppManager.java
@@ -107,7 +107,7 @@ public class WebAppManager {
 
 	private class DirFileFilter implements FileFilter {
 		public boolean accept(File file) {
-			return file.isDirectory() && !file.getName().equals("CVS")
+			return file.isDirectory() && !file.getName().equals("CVS") 
 					&& !file.getName().startsWith(".");
 		}
 	}
@@ -142,7 +142,7 @@ public class WebAppManager {
 		try{
 			String homeDirPath = server.getConfig(SDLoader.KEY_SDLOADER_HOME);
 			// webappsの絶対パス
-			webappDirPath = homeDirPath + "/webapps";
+			webappDirPath = homeDirPath + "/" + WebConstants.WEBAPP_DIR_NAME;
 			File webappDir = new File(webappDirPath);
 			if (!webappDir.exists()) {
 				throw new RuntimeException("webapps directory not exists.path="+ webappDirPath);
@@ -266,15 +266,23 @@ public class WebAppManager {
 		
 		ServletMappingTag webAppListMappingTag = new ServletMappingTag();
 		webAppListMappingTag.setServletName(webAppListServletName);
-		webAppListMappingTag.setUrlPattern("/*");
+		webAppListMappingTag.setUrlPattern("/");
+		
 		WebXml webXmlTag = new WebXml();
 		WebAppTag webAppTag = new WebAppTag();
 		webAppTag.addServlet(webAppListServletTag);
 		webAppTag.addServletMapping(webAppListMappingTag);
 		webXmlTag.setWebApp(webAppTag);
-
+		
+		// Default servlet
+		String contextPath = "/";
+		String docBase = webappDirPath + "/" + WebConstants.ROOT_DIR_NAME;
+		setDefaultServlet(webXmlTag, docBase, contextPath);
+		
 		WebAppClassLoader webAppClassLoader = createWebAppClassLoader("");
 		WebApplication webapp = new WebApplication(webXmlTag,null,"/",webAppClassLoader,this);
+		
+		
 		return webapp;
 	}
 
