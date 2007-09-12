e9e0ba56feedcd9b220489c3da64e3dc2ef8e7f1
c95029 <c95029@2bd7e31b-6d2d-0410-b585-a5d07697af20>
2007-09-12 14:20:04 +0000

    ライセンスとauthor付与

--- trunk/SDLoader/src/main/java/sdloader/command/CommandFactory.java
+++ trunk/SDLoader/src/main/java/sdloader/command/CommandFactory.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright 2005-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package sdloader.command;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/**
+ * @author shot
+ * @author c9katayama
+ */
 public final class CommandFactory {
 
 	private static final Map<String, Command> commands = new HashMap<String, Command>();
