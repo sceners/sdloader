c6158e367ea29ecc59535ab59a55411b938c66bb
c95029 <c95029@2bd7e31b-6d2d-0410-b585-a5d07697af20>
2007-09-12 14:12:46 +0000

    HTTPの場合、HTTP用の文字列部分をとるように修正

--- trunk/SDLoader/src/main/java/sdloader/command/CommandFactory.java
+++ trunk/SDLoader/src/main/java/sdloader/command/CommandFactory.java
@@ -2,6 +2,8 @@ package sdloader.command;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class CommandFactory {
 
@@ -11,13 +13,25 @@ public final class CommandFactory {
 		commands.put(Command.RESTART.toString(), Command.RESTART);
 	}
 
+	private static Pattern httpPattern = Pattern.compile("(GET|POST) /(.*) HTTP/.*");
+	
 	public static void addCommand(Command command) {
 		if (command != null) {
 			commands.put(command.toString(), command);
 		}
 	}
 
-	public static Command getCommand(final String key) {
+	public static Command getCommand(String key) {
+		key = stripHttp(key);
 		return commands.get(key);
+	}
+
+	private static String stripHttp(final String key){
+		Matcher match = httpPattern.matcher(key);
+		if(match.find()){
+			return match.group(2);
+		}else{
+			return key;
+		}
 	}
 }
