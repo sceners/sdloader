1bddd69725e1cbccfad859354e988a6cbefa5fee
c95029 <c95029@2bd7e31b-6d2d-0410-b585-a5d07697af20>
2007-09-27 16:07:46 +0000

    イベントディスパッチャー追加 ライフサイクルイベント追加

---
+++ tags/RELEASE_v0_1_04/SDLoader/src/main/java/sdloader/j2ee/webxml/ServletMappingTag.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2005-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sdloader.j2ee.webxml;
+
+/**
+ * servlet-mappingタグ
+ * 
+ * @author c9katayama
+ */
+public class ServletMappingTag {
+
+	private String servletName;
+
+	private String urlPattern;
+
+	public ServletMappingTag() {
+		super();
+	}
+
+	public String getServletName() {
+		return servletName;
+	}
+
+	public void setServletName(String servletName) {
+		this.servletName = servletName;
+	}
+
+	public String getUrlPattern() {
+		return urlPattern;
+	}
+
+	public void setUrlPattern(String urlPattern) {
+		this.urlPattern = urlPattern;
+	}
+
+}
