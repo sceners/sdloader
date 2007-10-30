e14efadbba4cc5e85af041d628cae6ee9b455462
c95029 <c95029@2bd7e31b-6d2d-0410-b585-a5d07697af20>
2007-10-30 11:26:41 +0000

    URLに対してwarプロトコル部分を追加していた部分をやめ、 rootURLに対して!/を付与して新しいＵＲＬを作成するようにした。

--- trunk/SDLoader/src/main/java/sdloader/internal/resource/WarProtocolBuilder.java
+++ trunk/SDLoader/src/main/java/sdloader/internal/resource/WarProtocolBuilder.java
@@ -9,10 +9,37 @@ public class WarProtocolBuilder {
 
 	public static final String RESOURCE_SEPARATOR = "!";
 
+	public static URL createArchiveResourceURL(final URL archiveUrl, final String resourcePath) {
+		assertBothNotNull(archiveUrl,resourcePath);
+		final String urlStr = archiveUrl.toExternalForm();
+		StringBuilder builder = new StringBuilder();
+		builder.append(urlStr);
+		builder.append(RESOURCE_SEPARATOR);
+		if(!resourcePath.startsWith("/")) {
+			builder.append("/");
+		}
+		builder.append(resourcePath);
+		return createURL(builder);
+	}
+
+	public static URL innerjarUrl(final URL rootUrl, final String originalPath) {
+		assertBothNotNull(rootUrl, originalPath);
+		final String urlStr = rootUrl.toExternalForm();
+		StringBuilder builder = new StringBuilder();
+		builder.append(JarArchiveTypeResourceImpl.PROTOCOL);
+		builder.append(urlStr);
+		builder.append(RESOURCE_SEPARATOR);
+		if(!originalPath.startsWith("/")) {
+			builder.append("/");
+		}
+		builder.append(originalPath);
+		return createURL(builder);
+	}
+	
+	
 	public static URL classPathToUrl(final URL rootUrl, final String originalPath) {
 		assertBothNotNull(rootUrl, originalPath);
-		final String s = originalPath.replace(ClassTypeResource.WEBINF_PREFIX,
-				"");
+		final String s = originalPath;//originalPath.replace(ClassTypeResource.WEBINF_PREFIX,"");
 		final String urlStr = rootUrl.toExternalForm();
 		StringBuilder builder = new StringBuilder();
 		builder.append(WAR_PROTOCOL);
@@ -30,20 +57,6 @@ public class WarProtocolBuilder {
 		final String urlStr = rootUrl.toExternalForm();
 		StringBuilder builder = new StringBuilder();
 		builder.append(WAR_PROTOCOL);
-		builder.append(urlStr);
-		builder.append(RESOURCE_SEPARATOR);
-		if(!originalPath.startsWith("/")) {
-			builder.append("/");
-		}
-		builder.append(originalPath);
-		return createURL(builder);
-	}
-
-	public static URL innerjarUrl(final URL rootUrl, final String originalPath) {
-		assertBothNotNull(rootUrl, originalPath);
-		final String urlStr = rootUrl.toExternalForm();
-		StringBuilder builder = new StringBuilder();
-		builder.append(JarArchiveTypeResourceImpl.PROTOCOL);
 		builder.append(urlStr);
 		builder.append(RESOURCE_SEPARATOR);
 		if(!originalPath.startsWith("/")) {
